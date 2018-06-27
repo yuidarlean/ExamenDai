3d2e53340e57ea257dc8941ee9ba0225a0ecc020
Ricardo Carcasson <ricardo.carcasson@gmail.com>
2018-06-27 16:56:36 -0400

    Carpeta base Carpeta de assets con framework del sitio Login.php

---
+++ AnalisisDeMuestras/php/base/header.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8" />
+        <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0, shrink-to-fit=no' name='viewport' />
+        <title>Solicitudes y An&aacute;lisis de Muestras - ISP</title>
+        <link href="assets/css/material-dashboard.css?v=2.1.0" rel="stylesheet" />
+        <!--   Core JS Files   -->
+        <script src="assets/js/core/jquery.min.js" type="text/javascript"></script>
+        <script src="assets/js/core/popper.min.js" type="text/javascript"></script>
+        <script src="assets/js/core/bootstrap-material-design.min.js" type="text/javascript"></script>
+        <script src="assets/js/plugins/perfect-scrollbar.jquery.min.js"></script>
+
+        <!--  Google Maps Plugin    -->
+        <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_KEY_HERE"></script>
+
+        <!-- Chartist JS -->
+        <script src="assets/js/plugins/chartist.min.js"></script>
+
+        <!--  Notifications Plugin    -->
+        <script src="assets/js/plugins/bootstrap-notify.js"></script>
+
+        <!-- Control Center for Material Dashboard: parallax effects, scripts for the example pages etc -->
+        <script src="assets/js/material-dashboard.min.js?v=2.1.0" type="text/javascript"></script>        
+    </head>
